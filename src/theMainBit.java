cac0ffc0314662fa17e4d1be325013ad90dad86e
Unknown <xolopx@gmail.com>
2018-04-08 21:42:19 +1000

    Setup
    - Spreadsheet for tracking progress
    - Repo (duh) 
    - Intellij Project

---
+++ src/theMainBit.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Tom on 08-Apr-18.
+ /**
+ * Created by Tom on 19-Jan-18.
+ */
+public class theMainBit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Yah bitch...");
+    }
+}
